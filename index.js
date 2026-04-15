ad8cfb44dca37b2068f3382b586c77d6e9b67981
Louis Gitonga <louisgitonga19@gmail.com>
2026-04-15 11:39:24 +0300

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+require('datejs');
 
+function combineUsers(...args) {
+    let combinedObject = {
+        users: []
+    };
 
+    for (let arr of args) {
+        combinedObject.users = [...combinedObject.users, ...arr];
+    }
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+    return combinedObject;
+}
+
+module.exports = { combineUsers };
